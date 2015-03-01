6a30c6e04dcec0c1c43cba6d851f657061edd2b5
Dominik Kundel <dominik.kundel@gmail.com>
2015-03-01 01:23:40 +0100

    working retrievment of the list

--- chrome-extension/app/scripts/tmplBackground.js
+++ chrome-extension/app/scripts/tmplBackground.js
@@ -1,12 +1,16 @@
 (function() {
   'use strict';
-  var ReminderUrls, createCleanUrl, getReminders, handlePageSwitch, localforage, requestSavedReminders, showNotification, storeReminder, _;
+  var ReminderUrls, Reminders, createCleanUrl, getReminders, handlePageSwitch, localforage, requestSavedReminders, showNotification, storeReminder, _;
 
   localforage = require('localforage');
 
   _ = require('underscore');
 
+  localforage.setDriver(localforage.LOCALSTORAGE);
+
   ReminderUrls = null;
+
+  Reminders = [];
 
   console.log('\'Allo \'Allo! Event Page for Browser Action');
 
@@ -59,8 +63,19 @@
     });
   };
 
-  getReminders = function(sendResponse) {
-    return [];
+  getReminders = function() {
+    Reminders = [];
+    return localforage.iterate(function(value, key) {
+      console.log('one more');
+      Reminders.push(value);
+    }, function() {
+      return chrome.runtime.sendMessage({
+        action: 'updateReminders',
+        reminders: Reminders
+      }, function() {
+        return console.log('responded');
+      });
+    });
   };
 
   handlePageSwitch = function(url, sendResponse) {
@@ -113,7 +128,10 @@
       handlePageSwitch(sender.url, sendResponse);
     }
     if ((request != null ? request.action : void 0) === 'addReminder') {
-      return storeReminder(request.reminder, sendResponse);
+      storeReminder(request.reminder, sendResponse);
+    }
+    if ((request != null ? request.action : void 0) === 'getReminders') {
+      return getReminders(sendResponse);
     }
   });
 

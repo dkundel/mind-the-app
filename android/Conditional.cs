bb73ffeecb4e2cf9da9c3e6d97ff0eb8ad1a4183
npentrel <npentrel@gmail.com>
2015-03-01 12:03:13 +0100

    nicer time picking

--- android/Conditional.cs
+++ android/Conditional.cs
@@ -53,26 +53,39 @@ namespace mindTheApp
 			private string app;
 			private string fullAppName = "Choose an app";
 			private static int id = 0;
+			private TextView time_display;
+			private Button pick_button;
+			private TextView time_display2;
+			private Button pick_button2;
+
+			private int hour;
+			private int minute;
+			private int hour2;
+			private int minute2;
+
+			const int TIME_DIALOG_ID = 0;
+			const int TIME_DIALOG_ID2 = 1;
 
 			protected void CreateTrigger(){
 
 				Switch toggleButton1 = FindViewById<Switch> (Resource.Id.toggleButton1);
 				Switch withRange = FindViewById<Switch> (Resource.Id.times);
-				TimePicker start = FindViewById<TimePicker> (Resource.Id.early);
-				TimePicker late = FindViewById<TimePicker> (Resource.Id.late);
+// 				TimePicker start = FindViewById<TimePicker> (Resource.Id.early);
+//				TimePicker late = FindViewById<TimePicker> (Resource.Id.late);
 				EditText msg = FindViewById<EditText> (Resource.Id.message);
 
-				if (toggleButton1.Checked) {
+					if (toggleButton1.Activated || toggleButton1.Checked) {
 
 					Action<Activity> a;
 
 					if (withRange.Checked) {
 
-						int h0 = start.CurrentHour.IntValue();
-						int hn = late.CurrentMinute.IntValue();
-						int m0 = start.CurrentHour.IntValue();
-						int mn = late.CurrentMinute.IntValue();
-					
+						int h0 = hour;
+						int hn = minute;
+						int m0 = hour2;
+						int mn = minute2;
+						Android.Util.Log.Info("num1"+ h0, "num2" + m0);
+
 						a = delegate(Activity obj) {
 							DateTime time = new DateTime ();
 							if (time.Hour >= h0 && time.Hour < hn
@@ -86,6 +99,7 @@ namespace mindTheApp
 									.SetDefaults(NotificationDefaults.Vibrate);
 								notificationManager.Notify (id, n.Build ());
 							}
+							Android.Util.Log.Info("num1"+ h0, "num2" + m0);
 						}; 
 					}else{
 						a = delegate(Activity obj) {
@@ -130,6 +144,58 @@ namespace mindTheApp
 				toggleButton1.CheckedChange += delegate {
 					this.CreateTrigger();
 				};
+
+				// Capture our View elements
+				time_display = FindViewById<TextView> (Resource.Id.early2);
+				time_display2 = FindViewById<TextView> (Resource.Id.late2);
+				pick_button = FindViewById<Button> (Resource.Id.pickTime);
+				pick_button2 = FindViewById<Button> (Resource.Id.pickTime2);
+				// Add a click listener to the button
+				pick_button.Click += (o, e) => ShowDialog (TIME_DIALOG_ID);
+				pick_button2.Click += (o, e) => ShowDialog (TIME_DIALOG_ID2);
+
+				// Get the current time
+				hour = DateTime.Now.Hour;
+				minute = DateTime.Now.Minute;
+
+				hour2 = DateTime.Now.Hour;
+				minute2 = DateTime.Now.Minute;
+
+
+				// Display the current date
+				UpdateDisplay ();
+			}
+			// Updates the time we display in the TextView
+			private void UpdateDisplay ()
+			{
+				string time = string.Format ("{0}:{1}", hour, minute.ToString ().PadLeft (2, '0'));
+				string time2 = string.Format ("{0}:{1}", hour2, minute2.ToString ().PadLeft (2, '0'));
+				time_display.Text = time;
+				time_display2.Text = time2;
+			}
+
+			private void TimePickerCallback (object sender, TimePickerDialog.TimeSetEventArgs e)
+			{
+				hour = e.HourOfDay;
+				minute = e.Minute;
+				UpdateDisplay ();
+			}
+
+			private void TimePickerCallback2 (object sender, TimePickerDialog.TimeSetEventArgs e)
+			{
+				hour2 = e.HourOfDay;
+				minute2 = e.Minute;
+				UpdateDisplay ();
+			}
+
+			protected override Dialog OnCreateDialog (int id)
+			{
+				if (id == TIME_DIALOG_ID)
+					return new TimePickerDialog (this, TimePickerCallback, hour, minute, false);
+				if (id == TIME_DIALOG_ID2)
+					return new TimePickerDialog (this, TimePickerCallback2, hour2, minute2, false);
+
+				return null;
 			}
 
 			protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
